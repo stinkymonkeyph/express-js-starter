7f46fe7463c471a7451073aabf99ca7074dd23bd
stinkymonkeyph <monkeydev.team@gmail.com>
2020-03-04 12:39:12 +0800

    add sample test endpoint

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ const express = require('express')
 const api = require('./api')
 const app = express()
 
-app.use('/', api)
+app.use('/api/v1', api)
+
+app.listen(3001, () => console.log('server running on port 3001'))
